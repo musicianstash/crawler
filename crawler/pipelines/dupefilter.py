1f47d2172c7efe47411febe8adbd62ece8827d5c
sitegroove <grabnarrok@gmail.com>
2016-04-16 14:11:19 +0200

    fixed dupe filter

--- crawler/pipelines/dupefilter.py
+++ crawler/pipelines/dupefilter.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from scrapy import signals
+from scrapy import signals, Field
 from scrapy.xlib.pydispatch import dispatcher
 from ..contrib.exceptions import DupeValidationException
 
@@ -15,37 +15,39 @@ class DupeFilterPipeline(object):
         if hasattr(spider, 'item') and hasattr(spider.item, 'unique_fields'):
             unique_fields = spider.item.unique_fields
             item_keys = spider.item().fields.keys()
+
             if unique_fields and not all(uf in item_keys for uf in unique_fields):
-                print("STOP SPIDER HERE")
+                raise Exception('Specified unique fields must match with item fields!')
             else:
+                if hasattr(spider.item, 'unique_code_field') and spider.item.unique_code_field:
+                    spider.item.fields['unique_code'] = Field()
+
                 self.unique_filter_enabled = True
 
     def process_item(self, item, spider):
         """Adds primary key of item to a given spider. """
-        return item
-        # if not self.unique_filter_enabled:
-        #     return item
+        if not self.unique_filter_enabled:
+            return item
 
-        code = self._create_unique_code(item)
+        unique_code = self._create_unique_code(item)
 
-        if code in self.unique_codes:
+        if unique_code in self.unique_codes:
             spider.crawler.stats.inc_value('dupe_exceptions')
-            message = 'Non unique item with code: {}'.format(code)
+            message = 'Non unique item with code: {}'.format(unique_code)
             raise DupeValidationException(message)
         else:
-            self.unique_codes.append(code)
+            self.unique_codes.append(unique_code)
+
+        if hasattr(item, 'unique_code_field') and item.unique_code_field:
+            item['unique_code'] = unique_code
 
         return item
 
     def _create_unique_code(self, item):
         values = []
 
-        for unique_field in self.unique_fields:
-            if unique_field not in item:
-                pass
-
-            values.append('' if item[unique_field] is None else item[unique_field])
+        for unique_field in item.unique_fields:
+            values.append('' if item[unique_field] is None else str(item[unique_field]))
 
         code = ''.join(values)
-        item['unique_code'] = code.replace(' ', '')
-        return item
+        return code.replace(' ', '').lower()
