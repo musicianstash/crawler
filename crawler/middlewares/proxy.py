56a17aaff8ca016dc0dffe29011bd3398020fe9d
sitegroove <grabnarrok@gmail.com>
2016-04-22 09:04:50 +0200

    proxy fix

--- crawler/middlewares/proxy.py
+++ crawler/middlewares/proxy.py
@@ -25,12 +25,11 @@ class ProxyDownloaderMiddleware(object):
         return cls(crawler.settings)
 
     def process_request(self, request, spider):
-        return
         # Don't overwrite with a random one (server-side state for IP)
         if 'proxy' in request.meta and not self.proxy_user_pass and self.proxy_address:
             return
 
         request.meta['proxy'] = self.proxy_address
         if self.proxy_user_pass:
-            basic_auth = 'Basic ' + base64.encodebytes(self.proxy_user_pass)
+            basic_auth = 'Basic ' + base64.b64encode(self.proxy_user_pass)
             request.headers['Proxy-Authorization'] = basic_auth
